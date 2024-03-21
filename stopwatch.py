74de3f75d773599bad7a8bd58c92f96935e1599c
ndelah <delahoussenicolas@gmail.com>
2024-03-21 09:40:01 +0100

    Create header

--- stopwatch.py
+++ stopwatch.py
@@ -1,7 +1,13 @@
 from textual.app import App, ComposeResult
+from textual.widgets import Header
 
 class StopwatchApp(App):
     """A Textual app that displays a stopwatch."""
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets here."""
+
+        yield Header()
 
 if __name__ == "__main__":
     app = StopwatchApp()
