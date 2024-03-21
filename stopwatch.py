ca132b0f56f3dc5a00d545d121040cf52b1b4901
ndelah <delahoussenicolas@gmail.com>
2024-03-21 10:03:37 +0100

    Add Start, Stop, Reset Buttons

--- stopwatch.py
+++ stopwatch.py
@@ -1,6 +1,19 @@
 from textual.app import App, ComposeResult
 from textual.containers import ScrollableContainer
-from textual.widgets import Header, Footer
+from textual.widgets import Header, Footer, Static, Button
+
+class Stopwatch(Static):
+    """A stopwatch widget"""
+
+    def compose(self) -> ComposeResult:
+        """Create child widget"""
+
+        yield Button("Start", id="start", variant="success")
+        yield Button("Stop", id="stop", variant="error")
+        yield Button("Reset", id="reset")
+        
+        
+
 
 class StopwatchApp(App):
     """A Textual app that displays a stopwatch."""
@@ -14,7 +27,7 @@ class StopwatchApp(App):
 
         yield Header()
         yield Footer()
-        yield ScrollableContainer()
+        yield ScrollableContainer(Stopwatch(),Stopwatch(),Stopwatch())
 
     def action_toggle_dark(self) -> None:
         """Toggle dark mode."""
